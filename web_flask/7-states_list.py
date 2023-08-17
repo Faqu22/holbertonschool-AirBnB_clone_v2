0a04deb08de8cd05861321d863949c1f76171cd0
Faqu22 <facundoalvarez415@gmail.com>
2023-08-17 15:12:25 -0300

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,65 +7,9 @@ from models.state import State
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """
-    Return 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Return 'HBNB'
-    """
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def displayC(text):
-    """
-    Return C with text.
-    """
-    text = text.replace('_', ' ')
-    return f"C {text}"
-
-
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def route3(text="is cool"):
-    """
-    Return Python with text.
-    """
-    return f"Python {text.replace('_', ' ')}"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def route4(n):
-    """
-    Return n is a number.
-    """
-    return f"{n} is a number"
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def route5(n):
-    """
-    Return a HTML with the variable n in h1.
-    """
-    return render_template('5-number.html', n=f"Number: {n}")
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def route6(n):
-    """
-        Return a HTML with the variable n in h1.
-    """
-    return render_template(
-        '6-number_odd_or_even.html',
-        n=f"Number: {n} is {'even' if n % 2 == 0 else 'odd'}")
+@app.teardown_appcontext
+def tearDown(arg):
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -75,9 +19,7 @@ def route7():
         states=storage.all(State).values()
     )
 
-@app.teardown_appcontext
-def tearDown(arg=None):
-    storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
